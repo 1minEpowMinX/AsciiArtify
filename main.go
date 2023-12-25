801572055fa4e3504acfc331b63b6b959510e8be
1minEpowMinX <129176682+1minEpowMinX@users.noreply.github.com>
2023-12-25 17:28:09 +0000

    Update main.go

--- main.go
+++ main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello World!")
-	})
-
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Hello World!")
 }
