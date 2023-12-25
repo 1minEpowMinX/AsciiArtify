b1fc72b5c9a9d572afb4dce743c20a90db8a8a5b
1minEpowMinX <129176682+1minEpowMinX@users.noreply.github.com>
2023-12-25 12:40:44 +0000

    Update main.go and add server

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Hello World!")
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
